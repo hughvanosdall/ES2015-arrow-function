297cfeaba6b3006f2333e003aa0c52b02392cddc
Hugh Vanosdall <hughvanosdall@gmail.com>
2022-08-09 00:41:09 -0700

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+const double = (arr) => arr.map((val) => val * 2);
+
+function squareAndFindEvens(numbers) {
+  let squares = numbers.map((num) => {
+    return num ** 2;
+  });
+  let evens = squares.filter((square) => {
+    return square % 2 === 0;
+  });
+  return evens;
+}
